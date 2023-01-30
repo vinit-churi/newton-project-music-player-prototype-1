01a9734e463076dd66c0bdc2c9cd01d946d3139d
vinit-churi <vinitchuri@hotmail.com>
2023-01-30 19:36:25 +0530

    before making another breaking changes

--- index.js
+++ index.js
@@ -15,32 +15,3 @@ body.addEventListener("click", (event) => {
 /************
  * HTML5 custom audio player
  ************/
-
-// pause and play icon toggle
-const playIcon = document.getElementById("play-icon");
-const pauseIcon = document.getElementById("pause-icon");
-
-[playIcon, pauseIcon].forEach((icon) => {
-    icon.addEventListener("click", () => {
-        playIcon.classList.toggle("hide-icon");
-        pauseIcon.classList.toggle("hide-icon");
-    });
-});
-
-const rangeInputs = document.querySelectorAll('input[type="range"]');
-
-function handleInputChange(e) {
-    let target = e.target;
-    if (e.target.type !== "range") {
-        target = document.getElementById("range");
-    }
-    const min = target.min;
-    const max = target.max;
-    const val = target.value;
-
-    target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
-}
-
-rangeInputs.forEach((input) => {
-    input.addEventListener("input", handleInputChange);
-});
